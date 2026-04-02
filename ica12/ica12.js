d3d8b29ad45ddccea4b6496b04c465dab7018755
jumi2082-jpg <jumi2082@colorado.edu>
2026-04-01 20:25:54 -0600

    Update ica12.js

--- ica12/ica12.js
+++ ica12/ica12.js
@@ -1,24 +1,38 @@
-let newBtn = document.querySelector('#js-new-qoute');
+
+let newBtn = document.querySelector('#js-new-quote');
+
+
+
 newBtn.addEventListener('click', getQuote);
 
+
 const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
- async function getQuote(){
-    try{
-       const response = await fetch(endpoint);
-       if (!response.ok){
-        throw Error (respone.statusText);
-       } 
-       const jason = await respinse.jasoon();
-       console.log (jason);
-       displayQuote(jason['question']);
-    } 
-    catch (err){
-        console.log(err)
-        alert('Failed to fetch now qoute');
+
+
+async function getQuote() {
+    try {
+        const response = await fetch(endpoint);
+
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        const json = await response.json();
+
+        console.log(json);
+
+        displayQuote(json.question);
+
+    } catch (err) {
+        console.log(err);
+        alert('Failed to fetch new quote');
     }
 }
-function displayQuote (quote){
-    const quoteText = document.querySelector
-    ('#js-qoute-text');
-    quoteText.textContent= quote;
+
+
+function displayQuote(quote) {
+    const quoteText = document.querySelector('#js-quote-text');
+    quoteText.textContent = quote;
 }
+
+getQuote();
